8b3049116ecadbad351550e3a00ff05854da8c05
CharlesWongg <821235825@qq.com>
2021-10-04 23:20:11 +0800

    observer methods

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,17 +1,48 @@
 import React from 'react'
-import { observer } from 'mobx-react'
+import { useObserver } from 'mobx-react'
 
-@observer
-class Todo extends React.Component {
-    render() {
-        const { todo, change } = this.props
-        return <div>
+// 方法一： 高阶函数
+// @observer
+// class Todo extends React.Component {
+//     render() {
+//         console.log('todo props')
+//         const { todo, change } = this.props
+//         return <div>
+//             <input type='checkbox' checked={todo.finished} onChange={() => {
+//                 change(todo)
+//             }} />
+//             {todo.title}
+//         </div>
+//     }
+// }
+
+// 方法二： Observer Component
+// class Todo extends React.Component {
+//     render() {
+//         console.log('todo props')
+//         const { todo, change } = this.props
+//         return <Observer>{() => {
+//             return <div>
+//                 <input type='checkbox' checked={todo.finished} onChange={() => {
+//                     change(todo)
+//                 }} />
+//                 {todo.title}
+//             </div>            
+//         }}</Observer>
+//     }
+// }
+
+// 方法三： useObserver Hook
+const Todo = ({ todo, change }) => {
+    console.log('todo props')
+    return useObserver(() => (
+        <div>
             <input type='checkbox' checked={todo.finished} onChange={() => {
                 change(todo)
             }} />
             {todo.title}
         </div>
-    }
+    ))
 }
 
 export default Todo
--- src/pages/TodoList.js
+++ src/pages/TodoList.js
@@ -5,18 +5,36 @@ import Todo from '../components/Todo'
 
 @observer
 class TodoList extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            input: '',
+            count: 0,
+        }
+    }
+
     render() {
         const { todoStore } = this.props
+        const { input, count } = this.state
         return (
             <div>
                 <h3>TodoList</h3>
+
+                <input type='text' value={input} onChange={(e) => this.setState({ input: e.target.value })} />
+                <button onClick={() => this.setState({ count: count + 1 })}>add Click {count}</button>
                 {todoStore.todos.map((todo) => (
                     <Todo key={todo.id} todo={todo} change={todoStore.changed}/>
                 ))}
-                <p>unfinishCount: {todoStore.unfinishCount}</p>           
+                <p>unfinishCount: {todoStore.unfinishCount}</p>
+                <Child count={{ count }}/>         
             </div>
         )
     }
 }
 
 export default TodoList
+
+const Child = React.memo((props) => {
+    console.log('child porps')
+    return <div>Child</div>
+}, (prevProps, nextProps) => prevProps.count.count === nextProps.count.count)
